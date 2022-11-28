1c195b6f8987f9e27fa25fab88cbac1d907359b9
Frank Trevino <fjoshuat@gmail.com>
2022-11-28 11:01:00 -0600

    heyyyyy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const form = document.querySelector("#create-task-form");
+
+  form.addEventListener("submit", (e) => createNewTask(e));
+
+  const createNewTask = (e) => {
+    e.preventDefault();
+    const talkDescription = document.querySelector(
+      "#new-task-description"
+    ).value;
+    const taskLi = document.createElement("li");
+    taskLi.innerText = talkDescription;
+    const taskUl = document.querySelector("#tasks");
+    taskUl.appendChild(taskLi);
+    form.reset();
+  };
 });
